879313407cbb818b2d240a32550ccf2eb99ad18d
Unknown <isabella.djurovic@gmail.com>
2017-10-24 15:42:23 -0400

    Added bfod UIasset files

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
+    bool thisthing;
+
 	// Use this for initialization
 	void Start () {
-		//wow coding sure is fun
+        //wow coding sure is fun
+        thisthing = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (thisthing == true) {
+
+        }
 	}
 }
